b9bb28c4a1977b21b0c64b7adf9f1ac4b46a8815
AMAAN ABDULNASSAR SHAIKH <46279971+amaan2398@users.noreply.github.com>
2022-06-18 13:03:33 +0530

    Linked List Cycle II

---
+++ Coding/Linked List/findBeginingOfCycleInLL/op1code.cpp
@@ -0,0 +1,98 @@
+/*
+@ Creator: Amaan Shaikh
+@ Problem_Title: findBeginingOfCycleInLL
+    @ Problem_Statement: Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null. There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter. Do not modify the linked list.
+    @ Data_Structure: Linked List
+    @ Algorithm: Hare and tortoise 
+    @ Level: Medium
+    @ URL: https://leetcode.com/problems/linked-list-cycle-ii/
+@ Approach: Optimized Approach 1
+@ Date_Time: June 18, 2022 13:00:05
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class ListNode{
+    public:
+    int data;
+    ListNode* next;
+    ListNode(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(ListNode* &Head, int val){
+    ListNode* node = new ListNode(val);
+    node->next = Head;
+    Head = node;
+}
+
+void insertAtTail(ListNode* &Head, int val){
+    if(Head == NULL){
+        insertAtHead(Head, val);
+        return;
+    }
+    ListNode* node = new ListNode(val);
+    ListNode* temp = Head;
+
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp-> next = node;
+}
+
+void display(ListNode* Head){
+    ListNode* temp = Head;
+    while(temp!= NULL){
+        cout << (temp->data) << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+void createCycle(ListNode* &head, int val){
+    ListNode* node;
+    ListNode* iter = head;
+    while(iter->next != NULL){
+        if(iter->data == val){
+            node = iter;
+        }
+        iter = iter->next;
+    }
+    iter->next = node;
+}
+
+ListNode *detectCycle(ListNode *head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+    do{
+        if((fast== NULL) or (fast->next == NULL)){
+            return NULL;
+        }
+        slow = slow->next;
+        fast = fast->next->next;
+    }while(slow!=fast);
+    fast = head;
+    while(fast != slow){
+        fast = fast->next;
+        slow = slow->next;
+    }
+    return slow;
+}
+
+int main(){
+    ListNode* Head = NULL;
+    insertAtTail(Head, 1);
+    insertAtTail(Head, 2);
+    insertAtTail(Head, 3);
+    insertAtTail(Head, 4);
+    insertAtTail(Head, 5);
+    display(Head);
+    createCycle(Head,3);
+    ListNode* node = detectCycle(Head);
+    cout << node->data << endl;
+    return 0;
+}
