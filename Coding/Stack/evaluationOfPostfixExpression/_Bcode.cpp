b6f40ae7f7fc6749c854c321ded602195ef80a6e
AMAAN ABDULNASSAR SHAIKH <46279971+amaan2398@users.noreply.github.com>
2022-07-27 01:54:29 +0530

    Evaluation of Postfix Expression

---
+++ Coding/Stack/evaluationOfPostfixExpression/_Bcode.cpp
@@ -0,0 +1,26 @@
+/*
+@ Creator: Amaan Shaikh
+@ Problem_Title: evaluationOfPostfixExpression
+    @ Problem_Statement: 
+    @ Data_Structure: 
+    @ Algorithm:
+    @ Level: 
+    @ URL: 
+@ Approach: Broad Force
+@ Date_Time: July 27, 2022 01:47:14
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void worker(){
+    cout << "Programmer!"<< endl;
+    return;
+}
+
+int main(){
+    cout << "Hello ";
+    worker();
+    return 0;
+}
---
+++ Coding/Stack/evaluationOfPostfixExpression/op1code.cpp
@@ -0,0 +1,65 @@
+/*
+@ Creator: Amaan Shaikh
+@ Problem_Title: evaluationOfPostfixExpression
+    @ Problem_Statement: 
+    @ Data_Structure: 
+    @ Algorithm:
+    @ Level: 
+    @ URL: 
+@ Approach: Optimized Approach 1
+@ Date_Time: July 27, 2022 01:47:14
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int evaluatePostfix(string S)
+{
+    // Your code here
+    stack<int> st;
+    int op1,op2;
+    int n = S.size();
+    for(int i = 0;i < n; i++){
+        switch(S[i]){
+            case '*': 
+                op2 = st.top();
+                st.pop();
+                op1 = st.top();
+                st.pop();
+                st.push(op1*op2);
+                break;
+            case '/': 
+                op2 = st.top();
+                st.pop();
+                op1 = st.top();
+                st.pop();
+                st.push(op1/op2);
+                break;
+            case '+':
+                op2 = st.top();
+                st.pop();
+                op1 = st.top();
+                st.pop();
+                st.push(op1+op2);
+                break;
+            case '-':
+                op2 = st.top();
+                st.pop();
+                op1 = st.top();
+                st.pop();
+                st.push(op1-op2);
+                break;
+            default:
+                st.push(S[i]-'0');
+        }
+    }
+    return st.top();
+}
+
+int main(){
+    string S;
+    cin >> S;
+    cout << evaluatePostfix(S);
+    return 0;
+}
