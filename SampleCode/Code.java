59d249746dea755e5d861e9d649fb8c353ef2f52
AMAAN ABDULNASSAR SHAIKH <46279971+amaan2398@users.noreply.github.com>
2022-08-19 23:26:42 +0530

    Created java sample code

---
+++ SampleCode/Code.java
@@ -0,0 +1,22 @@
+/*
+@ Creator: 
+@ Problem_Title: 
+    @ Problem_Statement: 
+    @ Data_Structure: 
+    @ Algorithm:
+    @ Level: 
+    @ URL: 
+@ Approach: 
+@ Date_Time: 
+*/
+
+// import java.util.*;
+
+class Code{
+    public static void worker(){
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args){
+        worker();
+    } 
+}
